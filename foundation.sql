19fc2b14fea98104cac2a6782e2ca8198d52c4d8
Isadhra <inderpreet.sadhra@nhs.net>
2025-04-30 15:08:51 +0000

    end of course

--- foundation.sql
+++ foundation.sql
@@ -12,9 +12,45 @@ SELECT
 FROM PatientStay ps
 WHERE ps.Hospital IN ('PRUH', 'Oxleas')
 AND ps.Ward LIKE '%Surgery'
-and ps.AdmittedDate BETWEEN '2024-02-27' AND '2024-03-01'
+--and ps.AdmittedDate BETWEEN '2024-02-27' AND '2024-03-01'
+order by ps.AdmittedDate DESC, ps.PatientId DESC
+
+
+SELECT 
+    ps.Hospital,
+    ps.Ward,
+    COUNT(ps.Tariff) AS CountTariff,
+    SUM(ps.Tariff) AS SumTariff,
+    AVG(ps.Tariff) AS AverageTariff,
+    MIN(ps.Tariff) AS MinTariff,
+    MAX(ps.Tariff) AS MaxTariff
+FROM PatientStay ps
+GROUP BY ps.Hospital, ps.Ward
+order by ps.numberOfPatients DESC
+--SELECT * FROM PatientStay ps
+
+
+
+
+
+select * from DimHospital
+
 --and ps.AdmittedDate >= '2024-02-27'
 --and ps.AdmittedDate <= '2024-03-01'
 
 
 --SELECT * FROM airports
+
+
+SELECT
+    ps.PatientId
+    ,ps.AdmittedDate
+    ,ps.Hospital
+    ,dh.HospitalType
+    ,dh.HospitalSize
+FROM
+    PatientStay ps INNER JOIN DimHospital dh
+    ON ps.Hospital = dh.Hospital
+where dh.hospital is NULL
+
+select * from DimHospitalBad
